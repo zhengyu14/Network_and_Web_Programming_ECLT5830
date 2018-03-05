11d69b237fc8aacf0537785840e1d23d22c59968
zhengyu14 <jan25th@live.cn>
2018-03-05 18:07:09 +0800

    view/index.php

--- asgn3/php/view/index.php
+++ asgn3/php/view/index.php
@@ -3,7 +3,28 @@
   include_once('top.php');
  ?>
 
- Content of home page would goes here ...
+<!-- Solutions goes here: -->
+<div class="container">
+<?php
+  // Read student ID, name from database
+  $sql = "SELECT user_id, first_name, last_name FROM users";
+  $studentInfo = mysqli_query($db, $sql);
+
+  // Print strudents information
+  if ( $studentInfo->num_rows > 0  ) {
+    while( $row = $studentInfo->fetch_assoc() ) {
+      echo '<div class="row">';
+      echo '<img src=../img/' . $row['user_id'] . '.jpg />';
+      echo '<div class="col"> Student Name: ' . $row['first_name'] . ' ' . $row['last_name'] . '</div>';
+      echo '<div class="col"> Student ID: ' . $row['user_id'] . '</div>';
+      echo "</div>";
+    }
+  } else {
+    echo "No result.";
+  }
+?>
+</div>
+<!-- End of sollution -->
 
 <?php
   include_once('bottom.php');
