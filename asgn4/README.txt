0539af11bc18109d684c62a846d26d9a135d70ae
zhengyu14 <jan25th@live.cn>
2018-03-27 09:50:08 +0800

    Asgn 4 Initial Load

---
+++ asgn4/README.txt
@@ -0,0 +1,8 @@
+asgn4-spec.pptx
+  Assignment specification
+
+task1/
+  Contains files needed for task 1
+
+task2/
+  Contains files needed for task 2
---
+++ asgn4/task2/getNames.php
@@ -0,0 +1,13 @@
+<?php
+  session_start();
+  require_once('data.php');
+
+  // TODO: Write code here to
+  // 1) Retrieve a string S sent from task2.html. Let this string be S.
+
+  // 2) Retrieve all strings in $names[] in which S is their prefix (case sensitive).
+
+  // Send the retrieved strings in the response (you have to figure out how to
+  // format these strings)
+  
+ ?>
