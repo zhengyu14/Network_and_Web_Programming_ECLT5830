13e2a2a05786328dbd08d2ecd08df6f106bf9510
zhengyu14 <jan25th@live.cn>
2018-03-29 10:16:19 +0800

    Assignment 4 SNAPSHOT 1.0.0

--- asgn4/task2/getNames.php
+++ asgn4/task2/getNames.php
@@ -4,10 +4,16 @@
 
   // TODO: Write code here to
   // 1) Retrieve a string S sent from task2.html. Let this string be S.
-
+  $S = (string)$_GET['prefix'];
+  $targets = [''];
   // 2) Retrieve all strings in $names[] in which S is their prefix (case sensitive).
-
+  for ($i=0; $i < count($names) ; $i++) {
+    if (strpos($names[$i], $S) === 0) {
+      array_push($targets, $names[$i]);
+    }
+  }
   // Send the retrieved strings in the response (you have to figure out how to
   // format these strings)
-  
+  header("Content-type: application/json");
+  echo json_encode($targets);
  ?>
